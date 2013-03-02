803d8ad8cf31a74c13cec0f68547cfdc56dae6dc
drealdal <meiqizhu@gmail.com>
2013-03-02 09:05:18 -0800

    test from string finish, and dump to list

--- json/jsonparser.py
+++ json/jsonparser.py
@@ -9,18 +9,35 @@ class JsonParser(object):
     '''
     classdocs
     '''
-    def __init__(self,str=None):
-        '''
-        Constructor
-        '''
+    def load(self,str):
         try:
             if False == isinstance(str,basestring):
                 raise JsonError("Argument Not String")
         except JsonError,e:
             e.print_error()
             return None
-        self._str = self._format_(str);
-        self._print(self._str)
+        self._str = self._format_(str)
+        return self._parse()
+    def loadJson(self,file):
+        if file is None:
+            raise JsonError('File Path is Null')
+        try:
+            fd = open(file,'r')
+        except:
+            raise JsonError('Open File Error')
+        try:
+            str = fd.read()
+        except:
+            raise JsonError('Read Data Error')
+        self.load(str)
+       
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+       
+        self._str = None
         self._off = 0
         self._dict = {}
     def _parse(self):
@@ -41,9 +58,16 @@ class JsonParser(object):
                 raise JsonError("Format Error")
         except JsonError,e:
             e.print_error()
+            return False
+        self._strip()
+        if self._off < len(self._str):
+            print 'Extra Content After Json Text'
+            self._print_rest()
+            raise JsonError("Format Error")
+        return True
             
     def _parse_object(self):
-        print 'object'
+#        print 'object'
         self._strip()
         self._unexpected_end()
         object = {}
@@ -72,6 +96,7 @@ class JsonParser(object):
                     self._off+=1;
                 # "a":1234}
                     if c == '}':
+                        #ignore the ending '}'
                         break
                     elif c == ',':
                     # next begin of key
@@ -80,32 +105,32 @@ class JsonParser(object):
                         c = self._str[self._off]
                         self._off+=1
                 else:
-                    self._unexpected_char()
-                    
+                    self._unexpected_char()            
         return object
                 
     def _parse_array(self):
             a=[]
             self._strip()
             self._unexpected_end()
-            c = self._str
+            c = self._str[self._off]
             #empty array
             if c == ']':
+                self._off+=1
                 return a;
             while True:
                 v=self._parse_val();
                 a.append(v)
                 #ignore space between elements
                 self._strip()
+                self._unexpected_end()
                 c = self._str[self._off]
                 self._off+=1
                 if c == ']':
                     return a
                 elif c == ',':
-                    v = self._parse_val()
-                    a.append(v)
+                    continue
                 else:
-                    raise self._unexpected_char()
+                    self._unexpected_char()
         
 #def _parse_key(self):
         
@@ -120,25 +145,39 @@ class JsonParser(object):
             return v
         elif c == '{':
             self._off+=1
-            return self._parse_object()
-            return 
+            return self._parse_object() 
         elif c == '"':
             self._off += 1
             return self._strip_str()
+        elif c == 'n' and self._str[self._off: self._off + 4] == 'null':
+            self._off = self._off + 4
+            return None
+        elif c == 't' and self._str[self._off: self._off + 4] == 'true':
+            self._off = self._off + 4
+            return True
+        elif c == 'f' and self._str[self._off: self._off + 5] == 'false':
+            self._off = self._off + 5
+            return False
         else:
-            self._unexpected_char()
+            return self._strip_num()
+            
             
     def _strip(self):
         '''
         strip space, \t \n \r
         '''
-        while self._off < len(self._str) and self._str[self._off] in _STRIP_SET:
-            self._off+=1
+        while self._off < len(self._str):
+            c = self._str[self._off]
+            if self._str[self._off] in _STRIP_SET:
+                self._off+=1
+            else:
+                break
     #Reading string util " , or error
     def _strip_str(self):
         str=""
         begin=self._off
         while self._off < len(self._str):
+            c = self._str[self._off];
             if self._str[self._off] == '\\':
                 self._off += 1
                 #\ can not be last char
@@ -155,20 +194,59 @@ class JsonParser(object):
         return str
     #reading the numbers
     def _strip_num(self):
-        
-        end_str = _ESCAPE_STR + ',}]'
+        start = self._off
+        end_str = _QUOTES_ + ',}]'
+        while self._off < len(self._str)  and self._str[self._off] not in end_str:
+            self._off+=1
+        self._unexpected_end()
+            
+        num_str = self._str[start:self._off]
         try:
-            while self._ss[end] not in end_str:
-                end = end + 1
-            ns = self._ss[begin:end]
-            if '.' in ns or 'e' in ns or 'E' in ns or ns == 'int' or ns == '-inf':
-                n = float(ns)
+            if '.' in num_str or 'e' in num_str or 'E' in num_str or num_str == 'int' or num_str == '-inf':
+                num = float(num_str)
             else:
-                n = int(ns)
-            self._index = end
-            return n
-        except IndexError:
-            raise JsonParseError(_ERR_MSG, self._ss, self._index)
+                num = int(num_str)
+        except ValueError,e:
+            self._Wrong_Number_Format(num_str)
+        return num
+    
+    def _val_to_str(self,list,val):
+        if isinstance(val,type('')):
+            return val
+        elif isinstance(val, type({})):
+            return self._object_to_str(list, val)
+        elif isinstance(val, type([])):
+            return self._array_to_str(list, val)
+        elif val == True:
+            list.append('true')
+        elif val == False:
+            list.append('false')
+        elif val == None:
+            list.append('null')
+        else:
+            ss = str(val)
+        
+    def _kv_to_str(self,key,val):
+        return '\"' + key +'\":'+self._val_to_str(val)
+        
+    
+    def _object_to_str(self,list,object):
+        list.append("{")
+        keys = object.keys()
+        if len(keys) > 0:
+            list.append(self._kv_to_str(keys[0], object[keys[0]]))
+            for k in keys[1:]:
+                list.append(','+self._kv_to_str(k, object[k]))
+        list.append("}")
+    
+    def _array_to_str(self,list,array):
+        list.append('[')
+        if len(array) > 0:
+            list.append(array[0])
+            for k in array[1:]:
+                list.append(','+self._val_to_str(k))
+        list.append(']')
+        
     #strip the initial string
     def _format_(self,str=None):
         if str == None:
@@ -183,19 +261,33 @@ class JsonParser(object):
             raise JsonError("Unexpected Ending ")
     
     def _unexpected_char(self):
-            raise JsonError("Unexpected Char")
+        self._print_rest()
+        raise JsonError("Unexpected Char")
         
     def _unexpected_quote(self,c):
         if c not in _QUOTES_:
+            self._print_rest()
             raise JsonError("Unexpected Char After \\")
+    
+    def _Wrong_Number_Format(self,str):
+        self._print_rest()
+        raise JsonError("Wrong Number Format")
+    
+    def _print_rest(self):
+        print self._str[self._off:self._off+100]
+    def _dump_list(self):
+        list = []
+        if  isinstance(self._dict,type([])):
+            self._array_to_str(list, self._dict)
+        elif isinstance(self._dict,type('{}')):
+            self._object_to_str(list, self._dict)
     def print_dict(self):
-        print self._dict
+       print self._dict
         
 class JsonError(ValueError):
     
     def __init__(self,info):
         self._info =  info
     def print_error(self):
-        print "Error Info"
         print self._info
    
--- json/testParser.py
+++ json/testParser.py
@@ -3,37 +3,65 @@ Created on Mar 1, 2013
 
 @author: zhumeiqi
 '''
-from jsonparser import JsonParser
+from jsonparser import JsonParser,JsonError
 
-def test_wrong_type():
-    testdata=13
-    jpaser=JsonParser(testdata)
-    if jpaser==None :
-        print 'Wrong Type Pass'
-
-def test_basic_array():
-    testdata='[[]]'
-    jpaser=JsonParser(testdata)
-    assert jpaser != None
-    print 'Array Pass'
-def test_str_str_object():
-    testdata='{   "abc"  \n:\t"abc"}'
-    jpaser=JsonParser(testdata)
-    jpaser._parse()
-    assert jpaser != None
-    jpaser.print_dict()
-    
-    testdata='{   "abc"  \n:\t"abc" \t,\t"aa\\a":12}'
-    jpaser=JsonParser(testdata)
-    jpaser._parse()
-    jpaser.print_dict()
-    
+        
+def test_unit(str,Success,msg):
+    jpaser=JsonParser()
+    try:
+        result = jpaser.load(str)
+        if jpaser is None:
+            if Success == False:
+                print 'Test ' + msg + ' Success Result None'
+                exit()
+            else:
+                print 'Test ' + msg + ' Fail Result None'
+                exit()
+    except JsonError,e:
+        result = False;
+    if result == True and Success == False:
+        print 'Test ' + msg + ' Fail'
+        exit()
+    if result == False and Success == True:
+        print 'Test ' + msg + ' Fail'
+        exit()
+    print 'Test ' + msg + ' Success'
+    print jpaser.print_dict()
+        
     
 if __name__ == '__main__':
+    #wrong type
+#    testdata=13
+#    test_unit(testdata,False,'Wrong Type')
+    #array only
+    testdata=r'''[1]'''
+    test_unit(testdata,True,'Array Only')
+    testdata=r'''[[["key","key"]]][]'''
+    test_unit(testdata,False,'Array With Extra chars')
+    #basic key value pair
+    testdata=r'''{   "abc"  
+    :    "abc"}'''
+    test_unit(testdata,True,'key value pair')
+    #quotes
+    testdata=r'''{"controls": "\b\f\n\r\t"}'''
+    test_unit(testdata,True,'Quotes')
+    testdata=r'''{"controls": "\b\f\n\r\t\a"}'''
+    test_unit(testdata,False,'Wrong quotes')
+    #multi array
+    testdata=r'''[1,2,3,4,[1,2,3,4]]'''
+    test_unit(testdata,True,'multi elment array')
+   
+    testdata2 = r''' {
+        "integer": 1234567890,"real": -9876.543210,
+        "e": 0.123456789e-12,
+        "E": 1.234567890E+34,
+        "":  -23456789012E666,
+        "zero": 0,
+        "one": 1}'''
+    test_unit(testdata,True,'All in one')
     
-    test_wrong_type()
-    
-    test_basic_array()
-    
-    test_str_str_object()
+    test = {"a":"b","t":"f"}
+    str=''.join([','+k for k in test.keys()])
+    str=str[1:]
+    print str
     pass
